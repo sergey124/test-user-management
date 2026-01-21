e49d9ded668d4877920fd8bc800e007b5958fad4
sergey124 <voronovsd@gmail.com>
2026-01-22 00:04:24 +0100

    add a test psychologist for empty db

--- src/main/kotlin/com/psytech/Application.kt
+++ src/main/kotlin/com/psytech/Application.kt
@@ -1,10 +1,29 @@
 package com.psytech
 
+import com.psytech.entity.Psychologist
+import com.psytech.repository.PsychologistRepository
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import java.util.*
 
 @SpringBootApplication
-class Application
+class Application {
+	@Bean
+	fun initData(psychologistRepository: PsychologistRepository) = CommandLineRunner {
+		if (psychologistRepository.count() == 0L) {
+			val psychologist = Psychologist(
+				firstName = "John",
+				lastName = "Doe",
+				nationalSecurityNumber = "123-45-6789",
+				birthDate = Date(90, 0, 1),
+				organization = "Mental Health Clinic"
+			)
+			psychologistRepository.save(psychologist)
+		}
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<Application>(*args)
